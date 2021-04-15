b1ebb3221387ac996e025d2f5c97b3e06a77ee13
liujiaxinwow <liujiaxinwow@163.com>
2021-04-15 10:35:43 +0800

    test update

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ from torchtext.datasets import WikiText2
 from torchtext.data.utils import get_tokenizer
 from collections import Counter
 from torchtext.vocab import Vocab
+import time
 
 
 class TransformerModel(nn.Module):
@@ -74,6 +75,62 @@ def batchify(data, bsz, device):
     return data.to(device)
 
 
+def get_batch(source, i):
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+seq_len]
+    target = source[i+1:i+1+seq_len].reshape(-1)
+    return data, target
+
+
+def train():
+    model.train()
+    total_loss = 0.
+    start_time = time.time()
+    src_mask = model.generate_square_subsequent_mask(bptt).to(device)
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i)
+        optimizer.zero_grad()
+        if data.size(0) != bptt:
+            src_mask = model.generate_square_subsequent_mask(data.size(0)).to(device)
+
+        output = model(data, src_mask)
+        loss = criterion(output.view(-1, ntokens), targets)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        log_interval = 200
+        if batch % log_interval == 0 and batch > 0:
+            cur_loss = total_loss / log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches| '
+                  'lr {:0.2.2f} | ms/batch {:5.2f} | '
+                  'loss {:5.2f} | ppl {:8.2f}'.format(epoch, batch,
+                                                      len(train_data) // bptt,
+                                                      scheduler.get_last_lr()[0],
+                                                      elapsed * 1000 / log_interval,
+                                                      cur_loss, math.exp(cur_loss)))
+            total_loss = 0
+            start_time = time.time()
+
+
+def evaluate(eval_model, data_source):
+    eval_model.eval()
+    total_loss = 0
+    src_mask = model.generate_square_subsequent_mask(bptt).to(device)
+    with torch.no_grad():
+        for i in range(0, data_source.size(0) - 1, bptt):
+            data, targets = get_batch(data_source, i)
+            if data.size(0) != bptt:
+                src_mask = model.generate_square_subsequent_mask(data.size(0)).to(device)
+            output = eval_model(data, src_mask)
+            output_flat = output.view(-1, ntokens)
+            total_loss += len(data) * criterion(output_flat, targets).item()
+
+    return total_loss / (len(data_source) - 1)
+
+
 if __name__ == '__main__':
     train_iter = WikiText2(split='train')
     tokenizer = get_tokenizer('basic_english')
@@ -94,3 +151,45 @@ if __name__ == '__main__':
     train_data = batchify(train_data, batch_size, device)
     val_data = batchify(val_data, batch_size, device)
     test_data = batchify(test_data, eval_batch_size, device)
+
+    bptt = 35
+
+
+    ntokens = len(vocab.stoi)
+    emsize = 200
+    nhid = 200
+    nlayers = 2
+    nhead = 2
+    dropout = 0.2
+    model = TransformerModel(ntokens, emsize, nhead, nhid, nlayers, dropout).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    lr = 5.0
+    optimizer = torch.optim.SGD(model.parameters, lr=lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+    best_val_loss = float("inf")
+    epochs = 3
+    best_model = None
+
+    for epoch in range(1, epochs):
+        epoch_start_time = time.time()
+        train()
+        val_loss = evaluate(model, val_data)
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+              'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time), val_loss, math.exp(val_loss)))
+        print('-' * 89)
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_model = model
+
+        scheduler.step()
+
+        test_loss = evaluate(best_model, test_data)
+        print('=' * 89)
+        print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+            test_loss, math.exp(test_loss)
+        ))
+        print('=' * 89)
